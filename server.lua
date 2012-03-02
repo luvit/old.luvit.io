f4af9456e261a2574a45c387cb5c6a622e62d602
Tim Caswell <tim@creationix.com>
2012-03-02 22:31:12 +0000

    Fix tabs

--- server.lua
+++ server.lua
@@ -7,23 +7,23 @@ local PORT = 80
 
 http.createServer(stack.stack(
 
-	-- Redirect directories to index.html
-	function (req, res, continue)
+  -- Redirect directories to index.html
+  function (req, res, continue)
     if not req.uri then
       req.uri = url.parse(req.url)
     end
     local pathname = req.uri.pathname
     if pathname:sub(#pathname, 1) == "/" then
-    	pathname = pathname .. "index.html"
-    	req.uri.pathname = pathname
-    	req.url = pathname .. req.uri.search
+      pathname = pathname .. "index.html"
+      req.uri.pathname = pathname
+      req.url = pathname .. req.uri.search
     end
     continue()
-	end,
+  end,
 
-	-- Serve static files
+  -- Serve static files
   require('static')('/', {
-		directory = __dirname,   -- root of static server
+    directory = __dirname,   -- root of static server
     max_age = 24*60*60*1000, -- cache for 1 day
     is_cacheable = function (file)
       return true
