600ff26c7f78db72df1160140831c7f3ba45609c
Tim Caswell <tim@creationix.com>
2012-03-02 21:16:14 +0000

    Use stack-static from dvv

--- server.lua
+++ server.lua
@@ -1,11 +1,35 @@
 local http = require 'http'
 local stack = require 'stack'
-local stackStatic = require 'stack-static'
+local stackStatic = require 'static'
+local url = require 'url'
 
 local PORT = 80
 
 http.createServer(stack.stack(
-  stackStatic.new("/", __dirname, "index.html")
+
+	-- Redirect directories to index.html
+	function (req, res, continue)
+    if not req.uri then
+      req.uri = url.parse(req.url)
+    end
+    local pathname = req.uri.pathname
+    if pathname:sub(#pathname, 1) == "/" then
+    	pathname = pathname .. "index.html"
+    	req.uri.pathname = pathname
+    	req.url = pathname .. req.uri.search
+    end
+    continue()
+	end,
+
+	-- Serve static files
+  require('static')('/', {
+		directory = __dirname,   -- root of static server
+    max_age = 24*60*60*1000, -- cache for 1 day
+    is_cacheable = function (file)
+      return true
+    end,                     -- can cache served files
+  })
+
 )):listen(PORT, function ()
   print("luvit.io listening on http://luvit.io:" .. PORT .. "/")
 end)
