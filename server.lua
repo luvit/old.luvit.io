a65e223d638bd6c4efd7567f408c8f3a1d4cbb95
Tim Caswell <tim@creationix.com>
2012-08-29 17:54:29 +0000

    Rewrite server using web module

--- server.lua
+++ server.lua
@@ -1,22 +1,32 @@
-local http = require 'http'
-local stack = require 'stack'
-local url = require 'url'
-
+local pathJoin = require('path').join
+local root = pathJoin(__dirname, 'public')
+local createServer = require('web').createServer
 local PORT = process.env.PORT or 8080
 
-http.createServer(stack.stack(
+-- Define a simple custom app
+local function app(req, res)
+  if req.url.path == "/greet" then
+    return res(200, {
+      ["Content-Type"] = "text/plain",
+      ["Content-Length"] = 12
+    }, "Hello World\n")
+  end
+  res(404, {
+    ["Content-Type"] = "text/plain",
+    ["Content-Length"] = 10
+  }, "Not Found\n")
+end
 
-  -- Serve static files
-  require('static')('/', {
-    root = __dirname,   -- root of static server
-    maxAge = 24*60*60*1000, -- cache for 1 day
-    autoIndex = "index.html",
-    follow = true,
-    isCacheable = function (file)
-      return true
-    end,                     -- can cache served files
-  })
+-- Serve static files and index directories
+app = require('static')(app, {
+  root = __dirname,
+  index = "index.html",
+  autoIndex = true
+})
+-- Log all requests
+app = require('log')(app)
+-- Add in missing Date and Server headers, auto chunked encoding, etc..
+app = require('cleanup')(app)
 
-)):listen(PORT, function ()
-  print("luvit.io listening on http://luvit.io:" .. PORT .. "/")
-end)
+local server = createServer("0.0.0.0", PORT, app)
+print("luvit.io listening on http://luvit.io:" .. server:getsockname().port .. "/")
